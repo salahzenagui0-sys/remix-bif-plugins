201ba782526a8c82067c231bd5c74b506114023b
drafish <xwlyy1991@163.com>
2024-04-24 12:59:43 +0800

    节点地址改变后清空已部署合约列表

--- libs/remix-ui/run-tab/src/lib/reducers/runTab.ts
+++ libs/remix-ui/run-tab/src/lib/reducers/runTab.ts
@@ -94,6 +94,10 @@ export const runTabReducer = (state: RunTabState = runTabInitialState, action: A
     return {
       ...state,
       bif: {...state.bif, ...action.payload},
+      instances: state.bif.nodeUrl === action.payload.nodeUrl ? state.instances : {
+        instanceList: [],
+        error: null
+      },
     };
   }
   
